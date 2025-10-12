1f57480df04b0ffda1a617172b52a1093ea6d807
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-10-13 05:26:48 +0800

    Remove unused `Middleware` configuration in `bootstrap/app.php` to simplify the application setup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: \dirname(__DIR__))
     ->withRouting(
@@ -12,9 +11,6 @@ return Application::configure(basePath: \dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(static function (Middleware $middleware): void {
-        //
-    })
     ->withExceptions(static function (Exceptions $exceptions): void {
         //
     })->create();
