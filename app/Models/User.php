6b34f927a084fc4029ec1cee8097d29f6eb8caa5
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-10-14 01:06:53 +0800

    Update `pint.json` rules for improved type declarations and clean up PHPDoc comments in `User.php`

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -28,8 +26,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
      */
     protected $hidden = [
         'password',
