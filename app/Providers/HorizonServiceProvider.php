6f0d0ab7ac6daae7ae1346550e4422ead73ce47e
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-10-14 08:30:27 +0800

    Add `#[Override]` attributes to service provider methods and simplify arrow functions in gates.

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -7,12 +7,14 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
+use Override;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     /**
      * Bootstrap any application services.
      */
+    #[Override]
     public function boot(): void
     {
         parent::boot();
@@ -27,11 +29,11 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      *
      * This gate determines who can access Horizon in non-local environments.
      */
+    #[Override]
     protected function gate(): void
     {
-        Gate::define('viewHorizon', static function (?User $user = null): bool {
+        Gate::define('viewHorizon', static fn(?User $user = null): bool
             // TODO: Add authorized emails here
-            return false;
-        });
+            => false);
     }
 }
