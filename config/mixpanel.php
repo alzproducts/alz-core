38e6e2f3c10712d7f4854c24fc32600080aa46c3
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-11-23 09:25:55 +0800

    Update Mixpanel config to support service account authentication and UTM campaign lookup table integration

--- config/mixpanel.php
+++ config/mixpanel.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'project_token' => env('MIXPANEL_PROJECT_TOKEN'),
     'base_url' => env('MIXPANEL_BASE_URL', 'https://api.mixpanel.com'),
+    'project_id' => env('MIXPANEL_PROJECT_ID'),
+    'service_account_username' => env('MIXPANEL_SERVICE_ACCOUNT_USERNAME'),
+    'service_account_password' => env('MIXPANEL_SERVICE_ACCOUNT_PASSWORD'),
+    'utm_campaign_lookup_table_id' => env('MIXPANEL_UTM_CAMPAIGN_LOOKUP_TABLE_ID'),
 ];
