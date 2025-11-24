bd16e620dd57f926d759ee7d1d227a8ed68c50e8
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-11-25 07:35:42 +0800

    Move `Console` commands to `Presentation` namespace for improved folder structure and organization consistency.

--- app/Presentation/Console/Commands/SyncAdSpendCommand.php
+++ app/Presentation/Console/Commands/SyncAdSpendCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Console\Commands;
+namespace App\Presentation\Console\Commands;
 
 use App\Presentation\Jobs\SyncGoogleAdsToMixpanelJob;
 use Illuminate\Console\Command;
