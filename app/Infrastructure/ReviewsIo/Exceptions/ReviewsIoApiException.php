7eb8741ebba28980f7172a2129b06b850ce1d9da
TomMurrayAlz <tom.murray@alzproducts.co.uk>
2025-11-26 06:04:33 +0800

    Remove `ReviewsIoApiException` as it is no longer used in the codebase.

--- app/Infrastructure/ReviewsIo/Exceptions/ReviewsIoApiException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Infrastructure\ReviewsIo\Exceptions;
-
-use App\Infrastructure\Exceptions\ApiException;
-
-final class ReviewsIoApiException extends ApiException {}
